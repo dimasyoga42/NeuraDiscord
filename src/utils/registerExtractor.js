c885578a9133c1374e049decfef2ff62a915f426
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-07 20:30:52 +0700

    perbaikan database

--- src/utils/registerExtractor.js
+++ src/utils/registerExtractor.js
@@ -1,9 +1,11 @@
 import { YoutubeiExtractor } from "discord-player-youtubei";
+import { DefaultExtractors } from "@discord-player/extractor";
 
 export const registerExtractor = async (player) => {
+  await player.extractors.loadMulti(DefaultExtractors);
   await player.extractors.register(YoutubeiExtractor, {
     streamOptions: {
-      useClient: "WEB",
+      useClient: "TV_EMBEDDED",
     },
   });
   console.log("youtubei loaded");
