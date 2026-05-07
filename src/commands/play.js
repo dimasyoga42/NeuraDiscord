ed39a10496cf754430d746d2b0ca422c92b2e51b
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-07 20:15:01 +0700

    perbaikan menu

--- src/commands/play.js
+++ src/commands/play.js
@@ -2,20 +2,16 @@ import { SlashCommandBuilder } from "discord.js";
 
 export default {
   name: "play",
-
   data: new SlashCommandBuilder()
     .setName("play")
     .setDescription("play musik")
     .addStringOption((option) =>
       option.setName("judul").setDescription("judul lagu").setRequired(true),
     ),
-
   async execute(interaction, client) {
     try {
       await interaction.deferReply();
-
       const query = interaction.options.getString("judul");
-
       const voiceChannel = interaction.member.voice.channel;
 
       if (!voiceChannel) {
@@ -34,42 +30,35 @@ export default {
         metadata: {
           channel: interaction.channel,
         },
-
         leaveOnEmpty: true,
         leaveOnEmptyCooldown: 300000,
-
         leaveOnEnd: true,
         leaveOnEndCooldown: 300000,
-
         leaveOnStop: true,
-
+        selfDeaf: true, // <-- tambahan
         volume: 80,
       });
 
       try {
         if (!queue.connection) {
-          await queue.connect(voiceChannel);
+          await queue.connect(voiceChannel, { deaf: true }); // <-- tambahan
         }
       } catch {
         queue.delete();
-
         return interaction.editReply("gagal join vc");
       }
 
       queue.addTrack(searchResult.tracks[0]);
 
       if (!queue.isPlaying()) {
-        queue.node.setVolume(100);
-
-        await queue.node.play();
+        await queue.node.play(); // <-- hapus setVolume, kadang bikin bug
       }
 
       await interaction.editReply(
-        `🎵 sekarang memutar:\n${searchResult.tracks[0].title}`,
+        `🎵 sekarang memutar:\n**${searchResult.tracks[0].title}**`,
       );
     } catch (err) {
       console.error(err);
-
       if (interaction.deferred) {
         await interaction.editReply("gagal memutar musik");
       }
