6e11ba3603e9bc83bef2d5cab9879d18ee940844
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-08 22:34:16 +0700

    penambahan bos

--- src/commands/bantuan.js
+++ src/commands/bantuan.js
@@ -11,17 +11,25 @@ export default {
     const data = await axios.get(
       "https://api.waifu.im/images?IncludedTags=waifu",
     );
+    const user = interaction.user;
     const img = data.data.items;
     const embed = new EmbedBuilder()
       .setColor(0x0099ff)
       .setTitle("Menu Neura Sama")
       .setThumbnail(img[0].url)
+      .setFooter("neura")
       .setTimestamp()
       .setFields([
         {
+          name: "Informasi Pribadi",
+          value: `
+          username: ${user.username}
+          name: ${interaction.member.displayName}
+          `,
+        },
+        {
           name: "Menu Toram",
-          value:
-            "- /xtal\n- /trait\n- /bos\n - /ava\n- /bossdef\n- /bosboost\n- /mt\n- /live",
+          value: "- /xtal\n- /trait\n- /bos\n- /boost\n- /itemfilter\n- /buff",
         },
       ]);
 
