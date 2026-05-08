92f461e532a1b8bcff846b8bb3a92eebb522768f
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-09 06:33:16 +0700

    penambahan

--- src/commands/buff.js
+++ src/commands/buff.js
@@ -156,7 +156,7 @@ export default {
           {
             name: "Code",
             value: data.code || "-",
-            inline: true,
+            inline: false,
           },
         ])
         .setTimestamp();
