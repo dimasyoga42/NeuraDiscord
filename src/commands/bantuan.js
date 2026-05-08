4de9c4b93fc4cee9b7ac08f4b0ae57162f7f9d41
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-08 22:36:03 +0700

    penambahan

--- src/commands/bantuan.js
+++ src/commands/bantuan.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+
 export default {
   name: "bantuan",
 
@@ -8,33 +10,66 @@ export default {
     .setDescription("Lihat semua menu"),
 
   async execute(interaction) {
-    const data = await axios.get(
-      "https://api.waifu.im/images?IncludedTags=waifu",
-    );
-    const user = interaction.user;
-    const img = data.data.items;
-    const embed = new EmbedBuilder()
-      .setColor(0x0099ff)
-      .setTitle("Menu Neura Sama")
-      .setThumbnail(img[0].url)
-      .setFooter("neura")
-      .setTimestamp()
-      .setFields([
-        {
-          name: "Informasi Pribadi",
-          value: `
-          username: ${user.username}
-          name: ${interaction.member.displayName}
-          `,
-        },
-        {
-          name: "Menu Toram",
-          value: "- /xtal\n- /trait\n- /bos\n- /boost\n- /itemfilter\n- /buff",
-        },
-      ]);
+    try {
+      await interaction.deferReply();
 
-    await interaction.reply({
-      embeds: [embed],
-    });
+      const { data } = await axios.get(
+        "https://api.waifu.im/search?included_tags=waifu",
+      );
+
+      const image = data.images?.[0]?.url;
+
+      const user = interaction.user;
+
+      const embed = new EmbedBuilder()
+        .setColor(0x00ffff)
+        .setTitle("Menu Neura Sama")
+        .setDescription(`Halo ${interaction.member.displayName} ✨`)
+        .setThumbnail(image)
+        .addFields([
+          {
+            name: "Informasi Pribadi",
+            value: [
+              `> Username: ${user.username}`,
+              `> Global Name: ${user.globalName || "-"}`,
+              `> Display Name: ${interaction.member.displayName}`,
+              `> User ID: ${user.id}`,
+            ].join("\n"),
+          },
+          {
+            name: "Menu Toram",
+            value: [
+              "`/xtal`",
+              "`/trait`",
+              "`/bos`",
+              "`/boost`",
+              "`/itemfilter`",
+              "`/buff`",
+            ].join("\n"),
+            inline: true,
+          },
+          {
+            name: "Menu Utility",
+            value: ["`/bantuan`", "`/ping`", "`/userinfo`"].join("\n"),
+            inline: true,
+          },
+        ])
+        .setImage(image)
+        .setFooter({
+          text: `Requested by ${user.username}`,
+          iconURL: user.displayAvatarURL(),
+        })
+        .setTimestamp();
+
+      await interaction.editReply({
+        embeds: [embed],
+      });
+    } catch (err) {
+      console.log(err);
+
+      if (interaction.deferred || interaction.replied) {
+        await interaction.editReply("terjadi kesalahan");
+      }
+    }
   },
 };
