d6dc25566d5d283726914e5ec5c4402cfc31df53
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-09 06:42:23 +0700

    perbaikan menu

--- src/commands/bantuan.js
+++ src/commands/bantuan.js
@@ -1,61 +1,25 @@
-import axios from "axios";
-
-import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { color } from "../config/color.js";
 
 export default {
-  name: "bantuan",
+  name: "help",
 
   data: new SlashCommandBuilder()
-    .setName("bantuan")
-    .setDescription("Lihat semua menu"),
+    .setName("help")
+    .setDescription("melihat menu"),
 
   async execute(interaction) {
-    try {
-      await interaction.deferReply();
-
-      const { data } = await axios.get(
-        "https://api.waifu.im/search?included_tags=waifu",
-      );
-
-      const image = data.images?.[0]?.url;
-
-      const user = interaction.user;
-
-      const embed = new EmbedBuilder()
-        .setColor(0x00ffff)
-        .setTitle("Menu Neura Sama")
-        .setDescription(`Halo ${interaction.member.displayName} ✨`)
-        .setThumbnail(image)
-        .addFields([
-          {
-            name: "Menu Toram",
-            value: [
-              "`/xtal`",
-              "`/trait`",
-              "`/bos`",
-              "`/boost`",
-              "`/itemfilter`",
-              "`/buff`",
-            ].join("\n"),
-            inline: false,
-          },
-        ])
-        .setImage(image)
-        .setFooter({
-          text: `Requested by ${user.username}`,
-          iconURL: user.displayAvatarURL(),
-        })
-        .setTimestamp();
-
-      await interaction.editReply({
-        embeds: [embed],
-      });
-    } catch (err) {
-      console.log(err);
-
-      if (interaction.deferred || interaction.replied) {
-        await interaction.editReply("terjadi kesalahan");
-      }
-    }
+    const enm = new EmbedBuilder()
+      .setColor(color.dark)
+      .setTitle("Neura Sama")
+      .setTimestamp()
+      .setDescription("Neura Sama akan selalu membantu anda")
+      .setFields([
+        {
+          name: "Menu Toram",
+          value: `- /itemfilter\n- /buff\n- /bos\n- /xtal\n- /trait\n- /boost`,
+        },
+      ]);
+    await interaction.EditReply("");
   },
 };
