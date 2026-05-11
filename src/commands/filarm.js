0e91a919c3d3e6466cc1384ab57f4947a5edf063
dimasyoga42 <dimasyoga9090@gmail.com>
2026-05-11 15:48:04 +0700

    perbaikan all message

---
+++ src/commands/filarm.js
@@ -0,0 +1,108 @@
+import axios from "axios";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+
+export default {
+  name: "filarm",
+
+  data: new SlashCommandBuilder()
+    .setName("filarm")
+    .setDescription("simulator filstat armor")
+    .addStringOption((option) =>
+      option
+        .setName("stat")
+        .setDescription("masukan stat yang anda inginkan")
+        .setRequired(true),
+    ),
+
+  async execute(interaction) {
+    const query = interaction.options.getString("stat");
+
+    const baseUrl = "https://neurapi.mochinime.cyou/api/toram/filarm?text=";
+
+    try {
+      const res = await axios.get(`${baseUrl}${encodeURIComponent(query)}`);
+
+      const data = res.data;
+
+      if (!data.ok || !data.hasValidResult) {
+        return await interaction.reply({
+          content: "stat tidak ditemukan atau hasil tidak valid",
+          ephemeral: true,
+        });
+      }
+
+      const stepsText = data.steps
+        .map((step, index) => `${index + 1}. ${step}`)
+        .join("\n\n");
+
+      const positiveStats = data.inputConfig.positiveStats
+        .map((s) => `${s.name} ${s.level}`)
+        .join("\n");
+
+      const negativeStats = data.inputConfig.negativeStats
+        .map((s) => `${s.name} ${s.level}`)
+        .join("\n");
+
+      const emb = new EmbedBuilder()
+        .setTitle("Simulator Filstat Armor")
+        .setDescription(`Simulator`)
+        .addFields(
+          {
+            name: "Positive Stats",
+            value: positiveStats || "-",
+            inline: true,
+          },
+          {
+            name: "Negative Stats",
+            value: negativeStats || "-",
+            inline: true,
+          },
+          {
+            name: "Success Rate",
+            value: data.successRate,
+            inline: true,
+          },
+          {
+            name: "Starting Potential",
+            value: data.startingPot,
+            inline: true,
+          },
+          {
+            name: "Material Cost",
+            value: data.materialCost,
+            inline: false,
+          },
+          {
+            name: "Highest Step Cost",
+            value: data.highestStepCost,
+            inline: true,
+          },
+          {
+            name: "Total Steps",
+            value: `${data.totalSteps}`,
+            inline: true,
+          },
+          {
+            name: "Steps",
+            value: stepsText.length > 1024 ? `${stepsText}` : stepsText,
+            inline: false,
+          },
+        )
+        .setFooter({
+          text: `Character Lv ${data.inputConfig.characterLevel} • PROF Lv ${data.inputConfig.professionLevel}`,
+        })
+        .setTimestamp(new Date(data.timestamp));
+
+      await interaction.reply({
+        embeds: [emb],
+      });
+    } catch (err) {
+      console.error(err);
+
+      await interaction.reply({
+        content: "terjadi kesalahan saat mengambil data dari Rest API",
+        ephemeral: true,
+      });
+    }
+  },
+};
